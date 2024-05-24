456b7470877c3c258827d7b424db371b918bf78d
Ezequiel <yejesquel@hotmail.com>
2024-05-24 18:48:31 -0500

    Mis practicas de python

---
+++ input.py
@@ -0,0 +1,13 @@
+# nombre = input("¿como te llamas? ")
+# print('Hola ' + nombre)
+
+# edad = input('¿cuantos años tienes? ')
+# print(type(edad))
+# print(f"{nombre} tiene {edad} años")
+
+# Programa que pide dos numeros y los suma
+numero1 = int(input("Introduce un número por favor :"))
+numero2 = int(input("Introduce otro número por favor :"))
+numero3 = numero1 + numero2
+
+print(f"El resultado de la suma es {numero3}")
---
+++ operadores.py
@@ -0,0 +1,37 @@
+# # operadores aritmeticos
+# print(2+2) #Suma
+# print(4-2) #Resta
+# print(3*7) #Multiplicacion
+# print(15/2) #Division
+# print(11%4) #Modulo
+# print(11 // 4) #División de piso
+# print(2**3) #Potencia
+
+# # Operadores en cadena de texto
+# print("hola" + "mundo") #Concatenación
+# print("Hola" * 3) #Repetición
+# print("Hola" + "mundo" * 3) #Concatetenación y repetición
+
+# Operadores de comparacion
+# print("Hola" == "hola") #Igual que
+# print("Hola" != "hola") #Distinto que
+# print(3 > 11) #Mayor o menor que 
+# print(11 >= 10) #Mayor o igual que 
+# print(10 <= 10) #Menor o igual que
+
+# Operadores de existencia 
+# print("ola" in "hola") #De existencia
+# print("ola" not in "hola") #De inexistencia
+
+# Operadores buleanos
+# print(True or False) #Or
+# print(True and False) #And
+
+# Operadores de asignación
+# x = 1 #Operedor de asignación estandar
+# x += 2 #Operedor de asignación suma
+# x *= 3 #Operador de asignación multiplicación 
+# x %= 4 #Operador de asignació módulo
+# print(x)
+x = 6 * 5 + 8 / 2 ** 4
+print(x)
